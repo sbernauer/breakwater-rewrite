70d92ae4b358865d1be90210e4916ccc6964dd95
Sebastian Bernauer <bernauerse@web.de>
2023-05-14 21:24:19 +0200

    Implement proper benchmarks

--- benches/parse_pixelflut_commands.rs
+++ benches/parse_pixelflut_commands.rs
@@ -1,35 +1,57 @@
 use breakwater::{
     framebuffer::FrameBuffer,
     parser::{parse_pixelflut_commands, ParserState},
-    test::helpers::DevNullTcpStream,
+    test::helpers::{get_commands_to_draw_rect, get_commands_to_read_rect, DevNullTcpStream},
 };
 use criterion::{
     BenchmarkId, Criterion, {criterion_group, criterion_main},
 };
 use std::sync::Arc;
 
+const FRAMEBUFFER_WIDTH: usize = 1920;
+const FRAMEBUFFER_HEIGHT: usize = 1080;
+
 async fn invoke_parse_pixelflut_commands(
     input: &[u8],
     fb: &Arc<FrameBuffer>,
-    parser_state: &ParserState,
+    parser_state: ParserState,
 ) {
     let mut stream = DevNullTcpStream::default();
-    parse_pixelflut_commands(input, fb, &mut stream, parser_state.clone()).await;
+    parse_pixelflut_commands(input, fb, &mut stream, parser_state).await;
 }
 
 fn from_elem(c: &mut Criterion) {
-    let input = [0_u8; 100000];
-    let fb = Arc::new(FrameBuffer::new(1920, 1080));
-    let parser_state = ParserState::default();
+    let draw_commands = get_commands_to_draw_rect(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, 0x123456);
+    let draw_commands = draw_commands.as_bytes();
 
     c.bench_with_input(
-        BenchmarkId::new("parse_pixelflut_commands", "default"),
-        &(&input, &fb, &parser_state),
-        |b, s| {
-            // Insert a call to `to_async` to convert the bencher to async mode.
-            // The timing loops are the same as with the normal bencher.
+        BenchmarkId::new(
+            "parse_draw_commands",
+            format!("{FRAMEBUFFER_WIDTH} x {FRAMEBUFFER_HEIGHT}"),
+        ),
+        &draw_commands,
+        |b, input| {
+            let fb = Arc::new(FrameBuffer::new(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT));
+            let parser_state = ParserState::default();
             b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| invoke_parse_pixelflut_commands(s.0, s.1, s.2));
+                .iter(|| invoke_parse_pixelflut_commands(input, &fb, parser_state.clone()));
+        },
+    );
+
+    let read_commands = get_commands_to_read_rect(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT);
+    let read_commands = read_commands.as_bytes();
+
+    c.bench_with_input(
+        BenchmarkId::new(
+            "parse_read_commands",
+            format!("{FRAMEBUFFER_WIDTH} x {FRAMEBUFFER_HEIGHT}"),
+        ),
+        &read_commands,
+        |b, input| {
+            let fb = Arc::new(FrameBuffer::new(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT));
+            let parser_state = ParserState::default();
+            b.to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter(|| invoke_parse_pixelflut_commands(input, &fb, parser_state.clone()));
         },
     );
 }
--- src/test/helpers/mod.rs
+++ src/test/helpers/mod.rs
@@ -1,5 +1,7 @@
 mod dev_null_tcp_stream;
 mod mock_tcp_stream;
+mod pixelflut_commands;
 
-pub use dev_null_tcp_stream::DevNullTcpStream;
-pub use mock_tcp_stream::MockTcpStream;
+pub use dev_null_tcp_stream::*;
+pub use mock_tcp_stream::*;
+pub use pixelflut_commands::*;
